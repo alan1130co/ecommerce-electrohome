a47876e8684e153a50eabf52da21f358a5b13ea4
alan coneo rodriguez <alanconeorodriguez1130@gmail.com>
2026-02-23 11:41:19 -0500

    Fix STATIC_URL con barra inicial

--- electrohome/Settings/production.py
+++ electrohome/Settings/production.py
@@ -18,7 +18,7 @@ CACHES = {
     }
 }
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

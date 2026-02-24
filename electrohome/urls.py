67ae46b19d60f06ecf64504cd892e7a0fba59d9c
alan coneo rodriguez <alanconeorodriguez1130@gmail.com>
2026-02-23 21:31:25 -0500

    Servir media en produccion

--- electrohome/urls.py
+++ electrohome/urls.py
@@ -9,11 +9,12 @@ urlpatterns = [
     path('order/', include('application.order.urls')),
     path('user/', include('application.user.urls')),
     path('chatbot/', include('application.chatbot.urls')),
-    path('dashboard/', include('application.dashboard.urls')),  # ← AGREGAR
+    path('dashboard/', include('application.dashboard.urls')),
     path('', include('application.product.urls')),
 ]
 
-# Archivos multimedia
+# Servir archivos media siempre (desarrollo y producción)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

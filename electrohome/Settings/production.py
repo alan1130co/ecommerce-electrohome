029495205344901622ab40cfcc0b2c642e41e46e
alan coneo rodriguez <alanconeorodriguez1130@gmail.com>
2026-02-24 19:39:28 -0500

    corregir configuracion email y database

---
+++ electrohome/Settings/production.py
@@ -0,0 +1,50 @@
+from .base import *
+import os
+import dj_database_url
+
+DEBUG = False
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('alanconeorodriguez1130@gmail.com', '')
+EMAIL_HOST_PASSWORD = os.environ.get('ahwq hgmk qpdf odbl', '')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+WOMPI_PUBLIC_KEY = os.environ.get('WOMPI_PUBLIC_KEY', '')
+WOMPI_PRIVATE_KEY = os.environ.get('WOMPI_PRIVATE_KEY', '')
+WOMPI_ENVIRONMENT = os.environ.get('WOMPI_ENVIRONMENT', 'test')
+WOMPI_API_URL = 'https://sandbox.wompi.co/v1'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+# ===== CLOUDINARY =====
+INSTALLED_APPS += ['cloudinary_storage', 'cloudinary']
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
